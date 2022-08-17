8625852adab797be66112d95f9ae43319f30bd50
choustark <1601459351@qq.com>
2022-08-17 21:37:18 +0800

    测试使用termanal 提交代码

--- spring-learn/src/main/java/com/chou/service/HelloSpringService.java
+++ spring-learn/src/main/java/com/chou/service/HelloSpringService.java
@@ -6,10 +6,9 @@ import org.springframework.stereotype.Service;
  * @author Axel
  * @version 1.0
  * @className HelleService
- * @description TODO
+ * @description Spring源码编译测试类...
  * @date 2022/8/14 19:04
  */
-
 @Service("helloSpringService")
 public class HelloSpringService {
 	public void sayHello(){
